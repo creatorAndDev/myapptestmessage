dc5e30dbac0c1c4be7917ed71204906f413e4c96
Yuri <creator.and.dev@gmail.com>
2017-10-30 21:19:21 +0200

    final

--- app/src/main/java/yuri/kl/com/ua/myapplication/MainActivity.java
+++ app/src/main/java/yuri/kl/com/ua/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package yuri.kl.com.ua.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("HI!");
+        textView.setTextSize(44);
+        textView.setTextColor(Color.RED);
+
+//        setContentView(R.layout.activity_main);
+        setContentView(textView);
     }
 }
